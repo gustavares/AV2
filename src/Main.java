613ea8f49e72a27c115ad3e5329378b7f3a079e5
Gustavo Tavares <gustavo.cardoso@al.infnet.edu.br>
2015-06-21 19:50:26 -0300

    v1.0

--- src/Main.java
+++ src/Main.java
@@ -15,8 +15,6 @@ public class Main {
 	}
 	
 	public static void main(String[] args) {
-//		TicketScanner ticketScannerObject = new TicketScanner();
-//		TicketPrinter ticketPrinterObject = new TicketPrinter();
 		Ticket ticket = new Ticket();
 		File file;
 		
